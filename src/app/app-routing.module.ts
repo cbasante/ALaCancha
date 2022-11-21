109c137165d2d5d1e34d3b4757e2476f98b722ed
Christyan Basante <cbasante@unipacifico.edu.co>
2022-11-20 21:08:38 -0500

    Se agrega página team y botones de navegabilidad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,10 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  {
+    path: 'team',
+    loadChildren: () => import('./team/team.module').then( m => m.TeamPageModule)
+  },
 ];
 
 @NgModule({
